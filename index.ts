aba0207c36135463dc2eb7bb5f5dfed24adfc8b1
Francisco Campos <sasasasamaes13@gmail.com>
2023-06-22 21:27:38 -0600

    se verifico y testeo y se notaron algunos cambios y validaciones de types

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const getUniqueNumbersArray = (n: number): number[] => {
   return arr;
 };
 
-console.log('uniqueNumbersArray =>',getUniqueNumbersArray(5));
+console.log('1 - uniqueNumbersArray =>',getUniqueNumbersArray(5));
 //  given a result of the previous method, generate an array of n numbers, then alternate them between positive and negative using a new function.
 
 // i: [0,1,2,3,4]
@@ -24,15 +24,19 @@ console.log('uniqueNumbersArray =>',getUniqueNumbersArray(5));
 // HERE
 
 const sortAlternatingNumbers = (arr: number[]): number[] => {
-  const sortedArr: number[] = [];
-  arr.forEach((index) => {
-    if (index % 2 === 0) {
-      sortedArr.push(index * -1);
+  const result: number[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 === 0) {
+      result.push(Math.abs(arr[i]) * -1);
     } else {
-      sortedArr.push(index);
+      result.push(Math.abs(arr[i]));
     }
-  });
-  return sortedArr;
+  }
+
+  result.sort((a, b) => a - b);
+
+  return result;
 };
 // Consider the following code:
 
@@ -44,7 +48,7 @@ const randomValues  = [
   [3,2],
   () => 'Hello World'
 ]
-
+console.log('2 - sortAlternatingNumbers =>',sortAlternatingNumbers(getUniqueNumbersArray(5)));
 //  create a function that infer parameters type, then return it ramdomly using the infered type.
 //  from the parameter type
 
@@ -57,27 +61,36 @@ function getRandomValue<T>(values: T[]): T {
 }
 
 const randomValue = getRandomValue(randomValues);
+console.log('3 - randomValue', randomValue);
 
 // consider the following code:
 
 const z = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const r = Math.ceil(Math.random() * 10);
+const r = Math.ceil(Math.random() * 10); // 1 - 10
 
 //  once you notice the posible error on it, make it visible using type checking. DONT FIX IT, just make it visible using a typescript error.
 
 // YOUR
 // CODE
-// HERE
-const z2: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const r2: number = Math.ceil(Math.random() * 10); // Error: Type 'number' is not assignable to type 'number[]'.
+// HERE;
+
+// Define a type for the valid index range of the array z
+type ZIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+// Try to assign r to a variable of type ZIndex
+const i: ZIndex = r; // TypeScript error if r is not in the range 0-9
+
+
 // EXTRA:
 //  consider the following code:
 const numbers = [5,2,3,4,1,6,7,8,9,10] as const
 
 //  create a function that infer the parameter and return a TYPECHECK of the elements SORTED.
-function sortAndCheckType<T extends readonly number[]>(numbers: T): typeof numbers {
-  const sortedNumbers = [...numbers].sort((a, b) => a - b);
-  return sortedNumbers as unknown as typeof numbers;
+function sortArray<T extends number>(arr: ReadonlyArray<T>): number[] {
+  const sortedArray = [...arr].sort((a, b) => a - b);
+  return sortedArray;
 }
 
-const sortedNumbers = sortAndCheckType(numbers);
+
+const sortedNumbers = sortArray(numbers);
+console.log('4 - sortedNumbers', sortedNumbers);
