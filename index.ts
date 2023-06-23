55c91f69567c2023d92bb1d5f741e60e5febfd1e
Francisco Campos <sasasasamaes13@gmail.com>
2023-06-22 19:53:18 -0600

    base

--- index.ts
+++ index.ts
@@ -5,7 +5,15 @@
 // YOUR
 // CODE
 // HERE
+const getUniqueNumbersArray = (n: number): number[] => {
+  const arr: number[] = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
 
+console.log('uniqueNumbersArray =>',getUniqueNumbersArray(5));
 //  given a result of the previous method, generate an array of n numbers, then alternate them between positive and negative using a new function.
 
 // i: [0,1,2,3,4]
@@ -15,6 +23,17 @@
 // CODE
 // HERE
 
+const sortAlternatingNumbers = (arr: number[]): number[] => {
+  const sortedArr: number[] = [];
+  arr.forEach((index) => {
+    if (index % 2 === 0) {
+      sortedArr.push(index * -1);
+    } else {
+      sortedArr.push(index);
+    }
+  });
+  return sortedArr;
+};
 // Consider the following code:
 
 const randomValues  = [
@@ -32,7 +51,12 @@ const randomValues  = [
 // YOUR
 // CODE
 // HERE
+function getRandomValue<T>(values: T[]): T {
+  const randomIndex = Math.floor(Math.random() * values.length);
+  return values[randomIndex];
+}
 
+const randomValue = getRandomValue(randomValues);
 
 // consider the following code:
 
@@ -44,9 +68,16 @@ const r = Math.ceil(Math.random() * 10);
 // YOUR
 // CODE
 // HERE
-
+const z2: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const r2: number = Math.ceil(Math.random() * 10); // Error: Type 'number' is not assignable to type 'number[]'.
 // EXTRA:
 //  consider the following code:
 const numbers = [5,2,3,4,1,6,7,8,9,10] as const
 
 //  create a function that infer the parameter and return a TYPECHECK of the elements SORTED.
+function sortAndCheckType<T extends readonly number[]>(numbers: T): typeof numbers {
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+  return sortedNumbers as unknown as typeof numbers;
+}
+
+const sortedNumbers = sortAndCheckType(numbers);
